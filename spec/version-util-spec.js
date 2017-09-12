2df4c74a9c42f1f8e9a2d8e66d04a9605fdee332
Joan Karadimov <joan.karadimov@gmail.com>
2017-09-12 23:15:32 +0300

    Implement a compareVersions function

--- spec/version-util-spec.js
+++ spec/version-util-spec.js
@@ -7,3 +7,22 @@ describe('parseVersion', function () {
     expect(parseVersion('4.20')).toEqual([4, 20]);
   });
 });
+
+describe('compareVersions', function () {
+  let compareVersions = versionUtil.compareVersions;
+
+  it('returns strcmp-style results for trivial versions', function () {
+    expect(compareVersions([1], [1])).toEqual(0);
+    expect(compareVersions([2], [1])).toBeGreaterThan(0);
+    expect(compareVersions([1], [5])).toBeLessThan(0);
+  });
+
+  it('ignores trailing zeroes', function () {
+    expect(compareVersions([1], [1, 0, 0])).toEqual(0);
+  });
+
+  it('handles versions with different lengths', function () {
+    expect(compareVersions([1, 2], [1])).toBeGreaterThan(0);
+    expect(compareVersions([1], [1, 5])).toBeLessThan(0);
+  });
+});
